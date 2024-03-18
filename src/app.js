93e0c5a4433d9d09b9cd87f1c628403cf0c915a9
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-03-19 00:33:17 +0530

    updates files

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import { Express } from "express";
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const app = Express()
+const app = Express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    Credentials: true
-}))
+    Credentials: true,
+  })
+);
 
-export { app }
+app.use(Express.json({ limit: "16kb" }));
+app.use(Express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(Express.static("public"))
+
+export { app };
