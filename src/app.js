5fc3d413c297cd06f6c01f9d267b77ea946db04f
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-03-23 06:13:35 +0530

    update files

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-import { Express } from "express";
+import  express  from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const app = Express();
+const app = express();
 
 app.use(
   cors({
@@ -11,8 +11,15 @@ app.use(
   })
 );
 
-app.use(Express.json({ limit: "16kb" }));
-app.use(Express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(Express.static("public"))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
